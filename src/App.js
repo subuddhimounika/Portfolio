18c190fc65b2c2cc08700b6bfe0169e3fffdc16e
subuddhimounika <mounika.subuddhi@gmail.com>
2025-09-08 00:54:16 +0530

    portfolio

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Portfolio from "./components/Portfolio"
+
+function App() {
+  return (
+   <div>
+    <Portfolio />
+   </div>
+  )
+}
+
+export default App;
